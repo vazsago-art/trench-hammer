3c3cd9c80ce66c91930d0a129aec7af8011628a6
vazsago-art <vazsago-art@users.noreply.github.com>
2026-04-02 23:24:52 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Trench Hammer Army Builder - Service Worker
 // Offline-first strategy: cache-first for assets, network-first for navigation
 
-const CACHE_NAME = 'trench-hammer-v20';
+const CACHE_NAME = 'trench-hammer-v21';
 const ASSETS_TO_CACHE = [
   '/trench-hammer/',
   '/trench-hammer/index.html',
